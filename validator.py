667d354690fb4ca5724c132f5f6acbf1eea29920
Matthew Yang <flamingmatthew@gmail.com>
2026-06-15 01:40:32 -0400

    Create validator.py
    Add validator stub with check function skeleton

---
+++ validator.py
@@ -0,0 +1,6 @@
+def check_naming(obj): ...      # SM_ / T_ / M_ convention
+def check_transforms(obj): ...  # frozen?
+def check_history(obj): ...     # construction history deleted?
+def check_pivot(obj): ...       # pivot at expected location?
+def check_uvs(obj): ...         # UV set present?
+def run_all(): ...
